26ed781e531f1ff4d1d8d9503500c13bad19366d
6838gamal <gamalalmaqtary6838@gmail.com>
2025-12-09 09:34:02 +0300

    po

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT", "3306")
+DB_PORT = os.getenv("DB_PORT", "13369")
 DB_NAME = os.getenv("DB_NAME")
 
 SECRET_KEY = os.getenv("SECRET_KEY", "CHANGE_ME")
